e0f83bd871140743b8f3df72cff8c4bd48a62621
saikishore2410 <kandisaikishore@gmail.com>
2025-10-03 17:53:04 +0530

    Initial commit - Online Quiz App

---
+++ AppTest.java
@@ -0,0 +1,12 @@
+git import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testMain() {
+        // Test the main method of the App class
+        // You can add assertions to verify the expected behavior
+        assertTrue(true); // Placeholder assertion
+    }
+}
